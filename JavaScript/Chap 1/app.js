ce3a6c9f0da0de8de57d5c971c94ae26ea0dacfe
Muhammad Naveed <hanifnaveed486@gmail.com>
2023-01-25 22:34:05 +0500

    JavaScript Assignment

---
+++ JavaScript/Chap 1/app.js
@@ -0,0 +1,21 @@
+//Exercise 1
+alert("Hello Welcome to my Web page");
+
+// Exercise 2
+// alert("Muhammad Naveed");
+
+
+// Exercise 3
+// alert("Hello! \nWelcome to Java Script.. ");
+
+// Exercise 4
+// alert("Hello! Welcome to Java Script..")
+
+// Exercise 5
+//Done//
+
+// Exercise 6
+// Done//
+
+// Exercise 7
+//Done//
