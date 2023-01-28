13e54512f2c4f4c7f5f9a84e66a07696558e92bf
Muhammad Naveed <hanifnaveed486@gmail.com>
2023-01-28 19:28:44 +0500

    Chap 2 Assignment

---
+++ JavaScript/Chap 2/app.js
@@ -0,0 +1,43 @@
+// //Exercise 1
+var username;
+
+//Exercise 2
+var myName = " Muhammad Naveed "
+// // console.log(myName);
+
+//Exercise 3
+var message;
+message = "Hello World";
+// alert(message);
+
+//Exercise 4
+var name = " Muhammad Naveed ";
+var age = "19 Years old";
+var course = "Certified Mobile Application Development";
+// alert(name);
+// alert(age);
+// alert(course);
+
+//Exercise 5
+var food = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+// alert(food);
+
+//Exercise 6
+var email = "hanifnaveed486@gmail.com";
+// alert(email);
+
+//Exercise 7
+var book = " A smarter way to learn JavaScript. ";
+// alert(book);
+
+//Exercise 8
+// document.write(" <h1>Yah! I can write HTML content through JavaScript</h1> ");
+
+//Exercise 9
+var x = " ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ ";
+// alert(x);
+
+
+
+
+
