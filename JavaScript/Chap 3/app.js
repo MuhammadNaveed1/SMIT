b97377923de545c48cac57204b03287cb1cbad18
Muhammad Naveed <hanifnaveed486@gmail.com>
2023-01-30 23:57:26 +0500

    Chap 3 Assignment

---
+++ JavaScript/Chap 3/app.js
@@ -0,0 +1,16 @@
+// Exercise 1
+var age = "i'm 19 years old";
+// alert(age);
+
+// Exercise 2
+var visited = "You have visited this site 20 times";
+// alert(visited);
+
+// Exercise 3
+var birthYear = "My birth year is 2003";
+// document.write(birthYear);
+// document.write(" <br />Data type of my declared variable is number");
+
+// Exercise 4
+var name = "Muhammad Naveed"; var productType = "T-shirt(s)"; var qauntity = "5";
+// document.write(name," ordered", " ",qauntity," ",productType," on SMIT Clothing store")
