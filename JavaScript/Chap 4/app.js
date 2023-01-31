742b9fb917dd25e010c8125b92477f25449535b4
Muhammad Naveed <hanifnaveed486@gmail.com>
2023-01-31 20:04:10 +0500

    Chap 4 Assignment

---
+++ JavaScript/Chap 4/app.js
@@ -0,0 +1,16 @@
+// Exercise 1
+var sss ; var var2; var var3;
+
+// Exercise 2
+// legal
+// var hello; var he110; var helloWorld; var h$ll_o; var HELLO;
+// illegal
+// var 1hello; var h ello; var hell-o; var var; var 123; var "hello";
+
+// Exercise 3
+// document.write("<h1>Rules for naming JS variables</h1 >")
+var num = " numbers,"; var dsign = " $"; var underScore = " _";
+// document.write("variable name can only contain ,",num,dsign," and",underScore," . For example : H_e11$0");
+// document.write(" </br > Variable must begin with a letter,",dsign," or ",underScore," . For example : hello, $hello or _hello");
+// document.write("</br > Variable names are case sensitive");
+// document.write(" </br > Variable names should not be JS keywords");
