8fde5c5e6c7ffa7cec8b6312d41db4ed594fe96f
Muhammad Naveed <hanifnaveed486@gmail.com>
2023-02-02 02:31:05 +0500

    Chap 5 Assignment

---
+++ JavaScript/Chap 5/app.js
@@ -0,0 +1,31 @@
+// // Exercise 1
+// var num1 = 20;
+// var num2 = 30;
+// var total = num1 + num2;
+// // document.write("Sum of " + num1 + " and " + num2 + " is " + total);
+
+// // Exercise 2
+// var num1 = 30;
+// var num2 = 20;
+// var subtotal = num1 - num2;
+// document.write("Subtraction of " + num1 + " and " + num2 + " is " + subtotal);
+// var multitotal = num1 * num2;
+// document.write(" </br > Multiplication of " + num1 + " and " + num2 + " is " + multitotal);
+// var divtotal = num1 / num2;
+// document.write(" </br > Division of " + num1 + " and " + num2 + " is " + divtotal);
+// var modtotal = num1 % num2;
+// document.write(" </br > Modulo of " + num1 + " and " + num2 + " is " + modtotal);
+
+// // Exercise 3
+// var num;
+// document.write("Value after variable declaration is " + num);
+// num = 50;
+// document.write(" </br > Initial value: " + num);
+// num++;
+// document.write(" </br> Value after increment is: " + num);
+// num = num + 7;
+// document.write(" </br > Value after addition is: " + num);
+// num--;
+// document.write(" </br > Value after decrement is: " + num);
+// num = num % 3;
+// document.write(" </br > The remainder is : " + num);
