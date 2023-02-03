9f9fd099deabb98a73d7c47469c980992fdc1fbf
Muhammad Naveed <hanifnaveed486@gmail.com>
2023-02-04 03:01:54 +0500

    Add Exercise 6 & 7

--- JavaScript/Chap 5/app.js
+++ JavaScript/Chap 5/app.js
@@ -37,3 +37,28 @@
 
 // Exercise 5
 // document.write("Table of 9 </br >" + "9x1=" + 9 * 1 + "</br > 9x2=" + 9 * 2 + "</br > 9x3=" + 9 * 3 + "</br > 9x4=" + 9 *4 + "</br > 9x5=" + 9 * 5 + "</br > 9x6=" + 9 * 6 + "</br > 9x7=" + 9 * 7 + "</br > 9x8=" + 9 * 8 + "</br > 9x9=" + 9 * 9 + "</br > 9x10=" + 9 * 10);
+
+// Exercise 6
+// var celsius = 15;
+// var fahrenheit = 59;
+// document.write("15°C is " + ((celsius * 9 / 5) + 32) + "°F");
+// document.write("</br > 59°F is " + ((fahrenheit - 32) * 5 / 9) + "°C");
+
+// Exercise 7
+// var item1 = 175;
+// var item2 = 260;
+// var ordQItem1 = 3;
+// var ordQItem2 = 6;
+// var shippingCharges = 270;
+// document.write("Price of item 1 is " + item1);
+// document.write("</br > Quantity of item 1 is " + ordQItem1);
+// document.write("</br > Price of item 2 is " + item2);
+// document.write("</br > Quantity of item 2 is " + ordQItem2);
+// document.write("</br > Shipping Charges " + shippingCharges);
+// // document.write("</br > Total cost of your order is " + (item1 + item1 + item1 + item2 + item2 + item2 + item2 + item2 + item2 + shippingCharges));
+
+// // OR
+
+// var item1Cost = item1 * ordQItem1;
+// var item2Cost = item2 * ordQItem2;
+// document.write("</br > Total cost of your order is " + (item1Cost + item2Cost + shippingCharges));
