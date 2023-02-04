f5a0b6e8e595fc32cbfa16942f9270ce642d8065
Muhammad Naveed <hanifnaveed486@gmail.com>
2023-02-04 20:07:27 +0500

    Chap 6 to 9 Assignment

---
+++ JavaScript/Chap 6 to 9/app.js
@@ -0,0 +1,33 @@
+// Exercie 1
+// var a = 9;
+// document.write("Result:");
+// document.write("<br />The value of a is: " + a);
+// document.write("<br /> <br /> <br /> The value of ++a is: " + ++a);
+// document.write("<br /> Now the value of a is:" + a);
+// document.write("<br /> <br /> <br /> The value of a++ is: " + a++);
+// document.write("<br /> Now the value of a is: " + a);
+// document.write("<br /> <br /> <br /> The value of --a is: " + --a);
+// document.write("<br /> Now the value of a is: " + a);
+// document.write("<br /> <br /> <br />  The value of a-- is: " + a--);
+// document.write("<br />  Now the value of a is: " + a);
+
+// Exercie 2
+// var a = 2;
+// var b = 1;
+// document.write("a is " + a);
+// document.write("<br /> b is " + b);
+// var result = --a - --b + ++b + b--;
+// document.write("<br /> result is " + result);
+
+// //Exercise 3
+// // alert("Hello Welcome to my Web page");
+// // var name = prompt("Please Enter Your Name");
+// // or
+// var name = prompt("Hello welcome to my Web page \nPlease Enter Your Name");
+
+// Exercise 4
+// NA
+
+// // Exercise 5
+// var num = +prompt("Enter Num", "5")
+// document.write(num * 1 + "<br />" + num * 2 + "<br />" + num * 3 + "<br />" + num * 4 + "<br />" + num * 5 + "<br />" + num * 6 + "<br />" + num * 7 + "<br />" + num * 8 + "<br />" + num * 9 + "<br />" + num * 10);
