a5962ec02ac9e7f35912ccc4bbc9e352b7c0769c
Muhammad Naveed <hanifnaveed486@gmail.com>
2023-02-06 18:30:47 +0500

    Chap 6 to 9 Assignment

--- JavaScript/Chap 6 to 9/app.js
+++ JavaScript/Chap 6 to 9/app.js
@@ -14,10 +14,17 @@
 // Exercie 2
 // var a = 2;
 // var b = 1;
+// var result = --a - --b + ++b + b--;
 // document.write("a is " + a);
 // document.write("<br /> b is " + b);
-// var result = --a - --b + ++b + b--;
 // document.write("<br /> result is " + result);
+// // Explaination Start
+// // --a = 1;
+// // --a - --b = 1 - 0;
+// // --a - --b + ++b = 1 - 0 + 1;
+// // --a - --b + ++b + b = 1 - 0 + 1 + 1;
+// // 1 - 0 + 1 + 1 = 3
+// //Explaination End
 
 // //Exercise 3
 // // alert("Hello Welcome to my Web page");
@@ -31,3 +38,45 @@
 // // Exercise 5
 // var num = +prompt("Enter Num", "5")
 // document.write(num * 1 + "<br />" + num * 2 + "<br />" + num * 3 + "<br />" + num * 4 + "<br />" + num * 5 + "<br />" + num * 6 + "<br />" + num * 7 + "<br />" + num * 8 + "<br />" + num * 9 + "<br />" + num * 10);
+
+//Exercise 6
+// var sub1 = "HTML"; var sub2 = "CSS"; var sub3 = "JS";
+// var sub1Marks = 84; var sub2Marks = 75; var sub3Marks = 95;
+// var totalMarks = 100;
+// document.write("<table cellpadding = '10px' align = 'center'>");
+//      document.write("<tr>");
+//           document.write("<th> Subject </th>");
+//           document.write("<th> Total Marks </th>");
+//           document.write("<th> Obtained Marks </th>");
+//           document.write("<th> Percentage </th>");
+//      document.write("</tr>");
+
+//      document.write("<tr>");
+//           document.write("<td>" + sub1 + "</td>");
+//           document.write("<td>" + totalMarks + "</td>");
+//           document.write("<td>" + sub1Marks + "</td>");
+//           document.write("<td>" + sub1Marks / totalMarks * 100 + "%" + "</td>");
+//      document.write("</tr>");
+
+//      document.write("<tr>");
+//           document.write("<td>" + sub2 + "</td>");
+//           document.write("<td>" + totalMarks + "</td>");
+//           document.write("<td>" + sub2Marks + "</td>");
+//           document.write("<td>" + sub2Marks / totalMarks * 100 + "%" + "</td>");
+//      document.write("</tr>");
+
+//      document.write("<tr>");
+//           document.write("<td>" + sub3 + "</td>");
+//           document.write("<td>" + totalMarks + "</td>");
+//           document.write("<td>" + sub3Marks + "</td>");
+//           document.write("<td>" + sub3Marks / totalMarks * 100 + "%" + "</td>");
+//      document.write("</tr>");
+     
+//      document.write("<tr>");
+//           document.write("<th>" + "" + "</th>");
+//           document.write("<th>" + (totalMarks + totalMarks + totalMarks) + "</th>");
+//           document.write("<th>" + (sub1Marks + sub2Marks + sub3Marks) + "</th>");
+//           document.write("<th>" + (sub1Marks + sub2Marks + sub3Marks) / 3 + "</th>");
+//      document.write("</tr>");
+
+// document.write("</table>");
