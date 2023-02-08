a54a52f18f24181bbebf02efaf6d6a901c52ff54
Muhammad Naveed <hanifnaveed486@gmail.com>
2023-02-09 02:54:43 +0500

    Chap 12 & 13 Assignment

---
+++ JavaScript/Chap 12 & 13/app.js
@@ -0,0 +1,147 @@
+// Exercise 1
+// var givenInput = prompt("Enter Character");
+// var char = givenInput.charCodeAt();
+// if (char >= 65 && char <= 90){
+//     alert("Uppercase letter");
+// }
+// else if (char >= 97 && char <= 122) {
+//     alert("Lowercase letter");
+// }
+// else if (char >= 48 && char <= 57) {
+//     alert("You have entered a number");
+// }
+
+// Exercise 2
+// var num1 = +prompt("Enter num1");
+// var num2 = +prompt("Enter num2");
+// if(num1 > num2) {
+//     alert(num1 + " is greater");
+// }
+// else if (num1 < num2){
+//     alert(num2 + " is greater");
+// }
+// else if (num1 === num2) {
+//     alert("Both numbers are equal");
+// }
+
+// // Exercise 3
+// num = +prompt("Enter a number");
+// if (num > 0) {
+//     alert("Positive number");
+// }
+// else if (num < 0) {
+//     alert("Negative number");
+// }
+// else if (num === 0) {
+//     alert("Zero");
+// }
+
+// exercise 4
+// var vowel = prompt("Enter a wovel")
+// if (vowel === "a" || vowel === "e" || vowel === "i" || vowel === "o" || vowel === "u" || vowel === "A" || vowel === "E" || vowel === "I" || vowel === "O" || vowel === "U") {
+//     alert("True");
+// }
+// else {
+//     alert("False");
+// }
+
+// exercise 5
+// var password = "abc654721@"
+// var enteredPass = prompt("Enter your password");
+// if (enteredPass === password) {
+//     alert("Correct!");
+// }
+// else if (enteredPass === "") {
+//     alert("Please enter your password");
+// }
+// else {
+//     alert("Incorrect password");
+// }
+
+// exercise 6
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// }
+// else{
+// greeting = "Good evening";
+// }
+
+// exercise 7
+// var time = +prompt("Enter time");
+// if (time === 100){
+//     alert("100 = 1am.");
+// }
+// else if (time === 200){
+//     alert("200 = 2am.");
+// }
+// else if (time === 300){
+//     alert("300 = 3am.");
+// }
+// else if (time === 400){
+//     alert("400 = 4am.");
+// }
+// else if (time === 500){
+//     alert("500 = 5am.");
+// }
+// else if (time === 600){
+//     alert("600 = 6am.");
+// }
+// else if (time === 700){
+//     alert("700 = 7am.");
+// }
+// else if (time === 800){
+//     alert("800 = 8am.");
+// }
+// else if (time === 900){
+//     alert("900 = 9am.");
+// }
+// else if (time === 1000){
+//     alert("1000 = 10am.");
+// }
+// else if (time === 1100){
+//     alert("1100 = 11am.");
+// }
+// else if (time === 1200){
+//     alert("1200 = 12am.");
+// }
+// else if (time === 1300){
+//     alert("1300 = 1pm.");
+// }
+// else if (time === 1400){
+//     alert("1400 = 2pm.");
+// }
+// else if (time === 1500){
+//     alert("1500 = 3pm.");
+// }
+// else if (time === 1600){
+//     alert("1600 = 4pm.");
+// }
+// else if (time === 1700){
+//     alert("1700 = 5pm.");
+// }
+// else if (time === 1800){
+//     alert("1800 = 6pm.");
+// }
+// else if (time === 1900){
+//     alert("1900 = 7pm.");
+// }
+// else if (time === 2000){
+//     alert("2000 = 8pm.");
+// }
+// else if (time === 2100){
+//     alert("2100 = 9pam.");
+// }
+// else if (time === 2200){
+//     alert("2200 = 10pm.");
+// }
+// else if (time === 2300){
+//     alert("2300 = 11pm.");
+// }
+// else if (time === 2400) {
+//     alert("2400 = 12am.");
+// }
+// else {
+//     alert("Not a time");
+// }
