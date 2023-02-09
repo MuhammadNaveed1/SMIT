e18e9556246b764ed5af390c3bc0a267e3ca476d
Muhammad Naveed <hanifnaveed486@gmail.com>
2023-02-10 01:04:22 +0500

    Correction of question 7

--- JavaScript/Chap 12 & 13/app.js
+++ JavaScript/Chap 12 & 13/app.js
@@ -71,76 +71,76 @@
 // exercise 7
 // var time = +prompt("Enter time");
 // if (time === 100){
-//     alert("100 = 1am.");
+//     alert("1am. \nGood night!");
 // }
 // else if (time === 200){
-//     alert("200 = 2am.");
+//     alert("2am. \nGood night!");
 // }
 // else if (time === 300){
-//     alert("300 = 3am.");
+//     alert("3am. \nGood night!");
 // }
 // else if (time === 400){
-//     alert("400 = 4am.");
+//     alert("4am. \nGood night!");
 // }
 // else if (time === 500){
-//     alert("500 = 5am.");
+//     alert("5am. \nGood morning!");
 // }
 // else if (time === 600){
-//     alert("600 = 6am.");
+//     alert("6am. \nGood morning!");
 // }
 // else if (time === 700){
-//     alert("700 = 7am.");
+//     alert("7am. \nGood morning!");
 // }
 // else if (time === 800){
-//     alert("800 = 8am.");
+//     alert("8am. \nGood morning!");
 // }
 // else if (time === 900){
-//     alert("900 = 9am.");
+//     alert("9am. \nGood morning!");
 // }
 // else if (time === 1000){
-//     alert("1000 = 10am.");
+//     alert("10am. \nGood morning!");
 // }
 // else if (time === 1100){
-//     alert("1100 = 11am.");
+//     alert("11pm. \nGood morning!");
 // }
 // else if (time === 1200){
-//     alert("1200 = 12am.");
+//     alert("12pm. \nGood afternoon!");
 // }
 // else if (time === 1300){
-//     alert("1300 = 1pm.");
+//     alert("1pm. \nGood afternoon!");
 // }
 // else if (time === 1400){
-//     alert("1400 = 2pm.");
+//     alert("2pm. \nGood afternoon!");
 // }
 // else if (time === 1500){
-//     alert("1500 = 3pm.");
+//     alert("3pm. \nGood afternoon!");
 // }
 // else if (time === 1600){
-//     alert("1600 = 4pm.");
+//     alert("4pm. \nGood afternoon!");
 // }
 // else if (time === 1700){
-//     alert("1700 = 5pm.");
+//     alert("5pm. \nGood afternoon!");
 // }
 // else if (time === 1800){
-//     alert("1800 = 6pm.");
+//     alert("6pm. \nGood afternoon!");
 // }
 // else if (time === 1900){
-//     alert("1900 = 7pm.");
+//     alert("7pm. \nGood evening!");
 // }
 // else if (time === 2000){
-//     alert("2000 = 8pm.");
+//     alert("8pm. \nGood evening!");
 // }
 // else if (time === 2100){
-//     alert("2100 = 9pam.");
+//     alert("9pm. \nGood night!");
 // }
 // else if (time === 2200){
-//     alert("2200 = 10pm.");
+//     alert("10pm. \nGood night!");
 // }
 // else if (time === 2300){
-//     alert("2300 = 11pm.");
+//     alert("11pm. \nGood night!");
 // }
 // else if (time === 2400) {
-//     alert("2400 = 12am.");
+//     alert("12am. \nGood night!");
 // }
 // else {
 //     alert("Not a time");
