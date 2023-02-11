da2746b9bd6179d711e750ddc64ea50c103207e4
Muhammad Naveed <hanifnaveed486@gmail.com>
2023-02-11 20:33:39 +0500

    Chap 14 to 16 Assignment

---
+++ JavaScript/Chap 14 to 16/app.js
@@ -0,0 +1,35 @@
+// Exercise 1
+// var studentNames = [ , , , , , ];
+// console.log(studentNames);
+
+// Exercise 2
+// var studentNames = new Array();
+// console.log(studentNames);
+
+// Exercise 3
+// var cities = ["Karachi", "Lahore", "Peshawar", "Hyderabad", "Multan"];
+// console.log(cities);
+
+// Exercise 4
+// var indexNumber = [0, 1, 2, 3, 4, 5];
+// console.log(indexNumber);
+
+// // Exercise 5
+// var booleanArray = [true, false];
+// console.log(booleanArray);
+
+// // Exercise 6
+// var mixedArray = [1, "two", false];
+// console.log(mixedArray);
+
+// // // Exercise 7
+// var qualitfications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil.", "PhD"];
+// document.write("1) " + qualitfications[0] + "<br /> 2) " + qualitfications[1] + "<br /> 3) " + qualitfications[2] + "<br /> 4) " + qualitfications[3] + "<br /> 5) " + qualitfications[4] + "<br /> 6) " + qualitfications[5] + "<br /> 7) " + qualitfications[6] + "<br /> 8) " + qualitfications[7]);
+
+// // // Exercise 8
+// var studentNames = ["Ali", "Ahmed", "Athar"];
+// var studentMarks = ["322", "377", "479"];
+// var totalMarks = 500;
+// document.write("Score of " + studentNames[0] + " is " + studentMarks[0] + ". Percentage: " + studentMarks[0] / totalMarks * 100);
+// document.write("<br /> Score of " + studentNames[1] + " is " + studentMarks[1] + ". Percentage: " + studentMarks[1] / totalMarks * 100);
+// document.write("<br /> Score of " + studentNames[2] + " is " + studentMarks[2] + ". Percentage: " + studentMarks[2] / totalMarks * 100);
