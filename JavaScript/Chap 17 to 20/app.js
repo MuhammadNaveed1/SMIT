28e357de4a5333d3b75b2427897730575e51a006
Muhammad Naveed <121456236+MuhammadNaveed1@users.noreply.github.com>
2023-02-18 19:27:55 +0500

    Chap 17 to 20 Assignment

---
+++ JavaScript/Chap 17 to 20/app.js
@@ -0,0 +1,19 @@
+// // Exercise 1
+// var arr = [[],[],[]];
+
+// Exercise 2
+// var arr = [[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 1]];
+// document.write("<h1>" + arr[0].join(" ") + "<br />" + arr[1].join(" ") + "<br />" + arr[2].join(" ") + "</h1>");
+
+// // Exercise 3
+// for (var i = 1; i <= 10; i++) {
+//     document.write( i + "<br />");
+// }
+
+// Exercise 4
+// var num = prompt("Enter number");
+// var loopLength = prompt("Enter loop length");
+// document.write("Multiplication table of " + num + " length " + loopLength + "<br />");
+// for (var i = 1; i <= loopLength; i++) {
+//     document.write(num + " x " + i + " = " + num * i + "<br />");
+// }
